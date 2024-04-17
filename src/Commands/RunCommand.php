bbcc291201623cdab0a340520288d227d88d38bd
Edipo Elwes <edipoelwes2@gmail.com>
2024-04-17 16:39:38 -0300

    🚀 Refactor RunCommand handle method
    ℹ️ Change RunCommand handle method to run multiple workers in the background.

--- src/Commands/RunCommand.php
+++ src/Commands/RunCommand.php
@@ -3,15 +3,34 @@
 namespace Edipoelwes\LaravelRabbitmqWorker\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\Process\Process;
 
 class RunCommand extends Command
 {
-    protected $signature = 'hello:world';
+    protected $signature = 'rabbitmq:run {workers}';
 
-    protected $description = 'Pequeno comando para demonstração sobre a criação de comandos no laravel';
+    protected $description = 'Runs processes in the background';
 
     public function handle()
     {
-        return $this->info('Olá Mundo dos pacotes no Laravel');
+        try {
+            $workers = explode(',', $this->argument('workers'));
+            $processes = [];
+
+            foreach ($workers as $worker) {
+                $process = new Process('php artisan worker:'.$worker);
+                $process->setTimeout(null);
+                $process->start();
+
+                $processes[] = $process;
+            }
+
+            foreach ($processes as $process) {
+                $process->wait();
+            }
+        } catch (\Throwable $th) {
+            Log::error(__METHOD__.' '.__LINE__, ['context' => $th->getMessage()]);
+        }
     }
 }
