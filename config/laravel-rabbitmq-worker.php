4d35291cc89487ec2959f491c7860064de946232
Edipo Elwes <edipoelwes2@gmail.com>
2024-04-22 16:50:19 -0300

    ✨🐛 Add configuration file for Laravel RabbitMQ worker
    📝 Created new configuration file for Laravel RabbitMQ worker with default
    settings.

---
+++ config/laravel-rabbitmq-worker.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'connections' => [
+        'host' => env('RABBITMQ_HOST', 'localhost'),
+        'port' => env('RABBITMQ_VHOST', 5672),
+        'user' => env('RABBITMQ_LOGIN', 'guest'),
+        'password' => env('RABBITMQ_PASSWORD', 'guest'),
+        'vhost' => env('RABBITMQ_VHOST', '/'),
+    ]
+];
