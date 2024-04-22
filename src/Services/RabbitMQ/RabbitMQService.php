07e1da988130634d347489b139716b2e528b49b1
Edipo Elwes <edipoelwes2@gmail.com>
2024-04-22 16:50:39 -0300

    ✨🐰 Add RabbitMQService class for publishing and consuming messages
    📦🐰 Add RabbitMQService class for publishing and consuming messages

---
+++ src/Services/RabbitMQ/RabbitMQService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Edipoelwes\LaravelRabbitmqWorker\Services\RabbitMQ;
+
+use Illuminate\Support\Facades\Log;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class RabbitMQService extends RabbitMQ
+{
+    public function __construct($queue, $routingKey, $exchange = '', $exchangeType = '') {
+        parent::__construct($queue, $routingKey, $exchange, $exchangeType);
+    }
+    public function publish($message)
+    {
+        $msg = new AMQPMessage($message, array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+        $this->channel->basic_publish($msg, $this->exchange, $this->routingKey);
+    }
+
+    public function consume(callable $callback, int $timeout = null)
+    {
+        $this->channel->basic_qos(null, 1, false);
+        $this->channel->basic_consume($this->queue, '', false, false, false, false, $callback);
+
+        try {
+            if($timeout) {
+                while ($this->channel->is_consuming())
+                    $this->channel->wait(null, false, $timeout);
+            } else {
+                $this->channel->consume();
+            }
+        } catch (\Throwable $th) {
+            Log::warning(__METHOD__.' '.__LINE__,  ['context' => $th->getMessage()]);
+        }
+    }
+}
