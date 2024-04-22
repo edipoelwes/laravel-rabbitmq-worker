5c17ef5cd18fcbe1233707fb3b0934a281384b8b
Edipo Elwes <edipoelwes2@gmail.com>
2024-04-22 16:50:46 -0300

    ✨ ⚙️ Add config file to publish and merge on CommandServiceProvider
    - Add config file to publish and merge in CommandServiceProvider for RabbitMQ
      worker configuration.

--- src/CommandServiceProvider.php
+++ src/CommandServiceProvider.php
@@ -9,11 +9,15 @@ class CommandServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Registre os recursos aqui, se necessário
+        $this->publishes([
+            __DIR__ . '/../config/laravel-rabbitmq-worker.php' => config_path('laravel-rabbitmq-worker.php'),
+        ], 'config');
     }
 
     public function register()
     {
+        $this->mergeConfigFrom( __DIR__ . '/../config/laravel-rabbitmq-worker.php', 'laravel-rabbitmq-worker');
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 RunCommand::class,
