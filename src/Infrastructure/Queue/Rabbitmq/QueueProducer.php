f9db7df05b8ed0088f5150515a0822c4942a6177
Edipo Elwes <edipoelwes2@gmail.com>
2026-04-22 21:15:04 -0300

    Enhance QueueProducer to support custom arguments for message publishing

--- src/Infrastructure/Queue/Rabbitmq/QueueProducer.php
+++ src/Infrastructure/Queue/Rabbitmq/QueueProducer.php
@@ -13,19 +13,23 @@ class QueueProducer
         $this->queueBuilder = $queueBuilder;
     }
 
-    public function produce(string $queueName, array $payload): void
+    public function produce(string $queueName, array $payload, array $arguments = []): void
     {
         $serializedPayload = json_encode($payload);
         $rabbitmqConnector = $this->queueBuilder->setQueueName($queueName)
-            ->setRouteKey($queueName)->getQueue();
+            ->setRouteKey($queueName)
+            ->setArguments($arguments)
+            ->getQueue();
         $rabbitmqConnector->publish($serializedPayload);
         $rabbitmqConnector->destruct();
     }
 
-    public function produceBatch(string $queueName, array $payload): void
+    public function produceBatch(string $queueName, array $payload, array $arguments = []): void
     {
         $rabbitmqConnector = $this->queueBuilder->setQueueName($queueName)
-            ->setRouteKey($queueName)->getQueue();
+            ->setRouteKey($queueName)
+            ->setArguments($arguments)
+            ->getQueue();
         $rabbitmqConnector->publishBatch($payload);
         $rabbitmqConnector->destruct();
     }
